19d15a7ae16fa557af0efb35a176fba58c43742a
irgiastadhani334 <irgiastadhani420@gmail.com>
2026-05-04 16:12:28 +0700

    Update GameManager.cpp

--- GameManager.cpp
+++ GameManager.cpp
@@ -6,6 +6,8 @@ void GameManager::runSession() {
 	Hand hand = handGenerator.generateHand();
 	handPlayer.playHand(hand);
 	int score = scoringRule.scoreHand(hand);
+	jokerCard.onHandPlayed(score);
+	score = jokerCard.modifyScore(score);
 	bool win = blindRule.checkBlind(score);
 	int reward = rewardRule.earnMoney(win, score);
 	std::cout << "Money gained: " << reward << "\n";
